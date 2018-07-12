6b8ba135216876584027b5df62e3f281b5774eb9
Katrin Leinweber <katrin.leinweber@tib.eu>
2018-07-12 13:56:57 +0200

    Initial git commit, started package

---
+++ R/center.R
@@ -0,0 +1,18 @@
+#' Centering Datasets
+#'
+#' Some more description.
+#'
+#' And even more.
+#'
+#' @param data The numeric vector to be centered
+#' @param desired The numeric midpoint value around which the data will be centered.
+#'
+#' @return A new vector containing the original data centered around the desired values
+#'
+#' @examples
+#'   center(c(1,2,3), 0) # should return [1] -1  0  1
+#'   center(c(1,2,3), 1) # should return [1] 0 1 2
+center <- function(data, desired) {
+  new_data <- (data - mean(data)) + desired
+  return(new_data)
+}
---
+++ R/rescale.R
@@ -0,0 +1,15 @@
+#' Rescaling Datasets
+#'
+#' @param v A numeric vector
+#'
+#' @return A new numeric vector of the same length, but rescaled from the input vector to the range of 0 to 1
+#'
+#' @examples
+#'   rescale(c(1,2,3)) # should return [1] 0.0 0.5 1.0
+#'   rescale(c(1,2,3,4,5)) # should return [1] 0.00 0.25 0.50 0.75 1.00
+rescale <- function(v) {
+  L <- min(v)
+  H <- max(v)
+  result <- (v - L) / (H - L)
+  return(result)
+}
