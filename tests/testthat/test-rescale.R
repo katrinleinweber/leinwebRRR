09d29dea85986454c0e673f014115161b4c94c1c
Katrin Leinweber <katrin.leinweber@tib.eu>
2018-07-12 16:56:06 +0200

    Test for an edge case
     / (upper - lower) + lower
    is has same result for lower = 1 and upper = 2 as
     * (upper - lower) + lower

--- tests/testthat/test-rescale.R
+++ tests/testthat/test-rescale.R
@@ -6,8 +6,12 @@ test_that("rescaling works, without overwriting the default", {
 })
 
 test_that("rescaling works with non-default arguments", {
-expect_equal(object = rescale(c(1, 2, 3),
-                              lower = 1,
-                              upper = 2),
-             expected = c(1.0, 1.5, 2.0))
+    expect_equal(object = rescale(c(1, 2, 3),
+                                  lower = 1,
+                                  upper = 2),
+                 expected = c(1.0, 1.5, 2.0))
+    expect_equal(object = rescale(c(1, 2, 3),
+                                  lower = 1,
+                                  upper = 4),
+                 expected = c(1.0, 2.5, 4.0))
 })
